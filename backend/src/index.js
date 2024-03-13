af53c18249e2ef6119f132ba231adc22f13843aa
Hussein Turfah <husseinturfah299@gmail.com>
2024-03-13 15:17:24 +0200

    Update CORS origin check in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,7 +30,13 @@ app.use(passport.initialize());
 
 app.use(
   cors({
-    origin: "*",
+    origin: function (origin, callback) {
+      if (origin === "http://192.168.1.254:3001") {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: "GET, POST, PUT, DELETE",
     credentials: true,
   })
