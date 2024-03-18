d0ddc7f922a3ee139450a728607ccaf7708f9057
Hussein Turfah <husseinturfah299@gmail.com>
2024-03-18 03:23:52 +0200

    Add file upload functionality using formidable and fs

--- backend/src/api/controllers/UploadController.js
+++ backend/src/api/controllers/UploadController.js
@@ -6,6 +6,9 @@ const {
 } = require("firebase/storage");
 const multer = require("multer");
 const path = require("path");
+const formidable = require("formidable");
+const fs = require("fs");
+const mv = require("mv");
 
 // Initialize Firebase storage
 // const storage = getStorage(require("../../config/firebaseConfig"));
@@ -70,4 +73,74 @@ const uploadFileLocally = (req, res) => {
   res.send({ success: true, filePath: req.file.path });
 };
 
-module.exports = { upload, uploadFile, uploadFileLocally };
+const uploadFolder = (req, res) => {
+  const form = new formidable.IncomingForm();
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      console.error("Error parsing form data:", err);
+      res.status(500).send("Error parsing form data");
+      return;
+    }
+
+    console.log("files:", files);
+
+    const folderName = fields.folderName;
+    const folderPath = `./uploads/${folderName}`;
+
+    fs.mkdir(folderPath, { recursive: true }, (err) => {
+      if (err) {
+        console.error("Error creating folder:", err);
+        res.status(500).send("Error creating folder");
+        return;
+      }
+
+      // Array to store asynchronous file move promises
+      const movePromises = [];
+
+      for (const fileArray of Object.values(files)) {
+        for (const file of fileArray) {
+          const oldPath = file.filepath;
+          const originalFilename = file.originalFilename.split("/").pop();
+          const newPath = `${folderPath}/${originalFilename}`;
+
+          // Move the file to the folder asynchronously
+          movePromises.push(
+            new Promise((resolve, reject) => {
+              mv(oldPath, newPath, (err) => {
+                if (err) {
+                  console.error("Error moving file:", err);
+                  reject(err);
+                } else {
+                  resolve();
+                }
+              });
+            })
+          );
+        }
+      }
+
+      // Wait for all file move promises to resolve
+      Promise.all(movePromises)
+        .then(() => {
+          res.send({
+            success: true,
+            message: "Folder uploaded successfully",
+            folderPath,
+          });
+        })
+        .catch((err) => {
+          console.error("Error moving files:", err);
+          res.status(500).send("Error moving files");
+        });
+    });
+  });
+};
+
+module.exports = {
+  upload,
+  uploadFile,
+  uploadFileLocally,
+  handleFileUpload,
+  uploadFolder,
+};
