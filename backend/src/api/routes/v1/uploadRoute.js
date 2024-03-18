5a8589b87274e52a35c43a0918de164d94bcd7c8
Hussein Turfah <husseinturfah299@gmail.com>
2024-03-18 03:24:02 +0200

    Add dependencies and routes for file upload

--- backend/src/api/routes/v1/uploadRoute.js
+++ backend/src/api/routes/v1/uploadRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { upload, uploadFile, uploadFileLocally } = require("../../controllers/UploadController");
+const { upload, uploadFile, uploadFileLocally, uploadFolder } = require("../../controllers/UploadController");
 
 router.post("/file", upload.single("file"), uploadFile);
 router.post("/localFile", upload.single("file"), uploadFileLocally);
+router.post("/localFolder", uploadFolder);
 
 module.exports = router;
